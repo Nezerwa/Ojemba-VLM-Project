3cefe58fa110c7b4327836ceb66e8863135c9a4e
Nezerwa <eligrand2000@gmail.com>
2023-08-16 20:18:07 +0200

    Extend maximum screen size measurement in Tailwind confing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.html"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      spacing: {
+        maxScreen: "calc((100% - 1280px) / 2)",
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
