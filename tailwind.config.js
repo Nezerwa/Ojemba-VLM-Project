4d1b91642497800dfd1cf0eb661126d35cba8f55
peninah98 <peninangizwena@gmail.com>
2023-08-16 20:17:32 +0200

    feat : hero section with fonts configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.html"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./src/**/*.html"],
+  theme: {
+      extend: {
+          fontFamily: {
+              'Roboto': ['Roboto', 'sans-serif'],
+              'shwishwi': ['Tilt Prism', 'cursive'],
+              'raleway': ['Raleway', "sans-serif"],
+              'montserrat': ['Montserrat', "sans-serif"]
+          }
+      },
+  },
+  plugins: [],
+}
