251c5d2988b19d3855172e157863ac4a15e54d94
Nezerwa <eligrand2000@gmail.com>
2023-08-17 12:40:46 +0200

    Extend fontfamily in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       spacing: {
         maxScreen: "calc((100% - 1280px) / 2)",
       },
+
+      fontFamily: {
+        'Roboto': ["Roboto", "sans-serif"],
+        'shwishwi': ["Tilt Prism", "cursive"],
+        'raleway': ["Raleway", "sans-serif"],
+        'montserrat': ["Montserrat", "sans-serif"],
+      },
     },
   },
   plugins: [],
