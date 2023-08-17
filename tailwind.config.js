d73e74added194a6175e91df63a18d87020d32a9
ll-sie <e.ndiramiye@alustudent.com>
2023-08-17 15:53:46 +0200

    feat: heroSection positioned Image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        maxScreen: "calc((100% - 1280px) / 2)",
+        maxScreen: "calc((100% - 1480px) / 2)",
       },
 
       fontFamily: {
